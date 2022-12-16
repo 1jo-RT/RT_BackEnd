cb1f82cbbcd49587a3ccbd79d1674f2e77c65b44
littlezero48 <littlezero048@email.com>
2022-12-16 19:09:29 +0900

    기본 세팅

---
+++ src/main/java/com/team1/rtback/RTbackApplication.java
@@ -0,0 +1,13 @@
+package com.team1.rtback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RTbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RTbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team1/rtback/RTbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team1.rtback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RTbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
