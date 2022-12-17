147b2e08c0e9323e9e556f482260d22c743aa2a4
baekgomsuyeom <jungin598@gmail.com>
2022-12-17 10:49:12 +0900

    feat: 게시글 댓글 기능 구현

---
+++ src/main/java/com/team1/rtback/dto/comment/CommentRequestDto.java
@@ -0,0 +1,11 @@
+package com.team1.rtback.dto.comment;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class CommentRequestDto {
+    private String comment;
+}
---
+++ src/main/java/com/team1/rtback/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.team1.rtback.repository;
+
+import com.team1.rtback.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
